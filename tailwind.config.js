d41f054c3fceecef2a86c04b773170e825c5e489
woolsey2316 <woolsey2316@gmail.com>
2020-12-21 13:58:30 +1100

    custom tailwind functionality now working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
-  fontFamily: {
-    'IBM-plex-sans': ['Nunito', 'sans-serif'],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
-  darkMode: "media", // or 'media' or 'class'
+  purge: ["./src/*.html"],
   theme: {
+    fontFamily: {
+      'IBM-plex-sans': ['IBM Plex Sans', 'sans-serif'],
+    },
     extend: {
       colors: {
-        informational: '#ff8a65',
-        warning: '#f2cc41',
-        danger: '#dc232e',
-        matrix_green: '#92cbba',
-        matrix_blue: '#3b5371',
-        true_blue: "#3968ae",
+        'informational': '#ff8a65',
+        'warning': '#f2cc41',
+        'danger': '#dc232e',
+        'matrixGreen': '#92cbba',
+        'matrixBlue': '#3b5371',
+        'trueBlue': "#3968ae",
         gray: {
           '100': '#ffffff',
           '200': '#f6fbfc',
@@ -25,30 +25,30 @@ module.exports = {
           '600': '#5c8697',
           '700': '#333333',
           '800': '#191919',
-          '900': '#000000',
+          '900': '#090909',
         },
-        spacing: {
-          'logo-w': '275px',
-          'logo-h': '57px',
-          '96': '24rem',
-          '108': '27rem',
-        },
-        margin: {
-          '1/2': '0.125rem',
-          '-px': '-1px',
-          '-2px': '-2px'
-        },
-        padding: {
-          '1/2': '0.125rem',
-          '-px': '-1px',
-          '-2px': '-2px'
-        },
-        backgroundImage: {
-          'spinning-stars': "url('./assets/images/spinning-stars.png')",
-          
-        }
+      },
+      spacing: {
+        'logo-w': '275px',
+        'logo-h': '57px',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem',
+      },
+      margin: {
+        '1/2': '0.125rem',
+        '-px': '-1px',
+        '-2px': '-2px'
+      },
+      padding: {
+        '1/2': '0.125rem',
+        '-px': '-1px',
+        '-2px': '-2px'
+      },
+      backgroundImage: {
+        'spinning-stars': "url('../images/spinning-stars.png')",
+        'object-over-earth': "url('../images/pexels-spacex-23789.png')",
       }
-      
     },
     screens: {
       'sm': '640px',
@@ -68,9 +68,6 @@ module.exports = {
     }
   
   },
-  variants: {
-    fontSize: ['responsive', 'hover'],
-    backgroundImage: ['responsive', 'hover', 'focus'],
-  },
-  plugins: []
-}
+  variants: {},
+  plugins: [],
+};
