0f9cd89a3a384c9c590d3a293d96a94d3bdf5b48
woolsey2316 <woolsey2316@gmail.com>
2020-12-22 20:01:35 +1100

    tablet design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,50 +3,83 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ["./src/*.html"],
+  // purge: ["./src/*.html"],
   theme: {
     fontFamily: {
       'IBM-plex-sans': ['IBM Plex Sans', 'sans-serif'],
     },
     extend: {
       colors: {
-        'informational': '#ff8a65',
+        'informational': '#3A6BAF',
         'warning': '#f2cc41',
         'danger': '#7C0C12',
         'matrix-green': '#92cbba',
-        'matrix-blue': '#3b5371',
+        'matrix-blue': '#3A68AF',
         'true-blue': "#3968ae",
         gray: {
-          '100': '#ffffff',
+          '100': '#eeeeee',
           '200': '#f6fbfc',
-          '300': '#eeeeee',
+          '300': '#9A9A9A',
+          '350': 'rgba(255,255,255,0.7)',
           '400': '#888888',
           '500': '#686868',
           '600': '#5c8697',
           '650': '#444444',
           '700': '#333333',
+          '750': '#272727',
           '800': '#191919',
           '900': '#090909',
         },
       },
+      borderWidth: {
+        '3': '3px',
+      },
+      lineHeight: {
+        '1': '15px',
+        '2': '18px',
+        '3': '23px',
+        '4': '25px',
+        '5': '27px',
+        '6': '35px',
+        '7': '53px',
+        '8': '62px'
+      },
+      fontSize: {
+        'xs': '13px',
+        'sm': '14px',
+        'md': '18px',
+        'lg': '19px',
+        'xl': '22px',
+        '2xl': '26px',
+        '3xl': '32px',
+        '4xl': '48px'
+      },
       spacing: {
-        'logo-w': '275px',
-        'logo-h': '57px',
+        '1/6': 'calc(16.66% - 1.5rem)',
+        '22': '5.75rem',
+        '26': '99px',
+        '35': '8.75rem',
         '84': '21rem',
         '96': '24rem',
         '108': '27rem',
+        '75': '19.4rem',
+        '120': '30rem',
+        '132': '33rem',
+        '133': '33.5rem',
+        '200px': '200px'
       },
       margin: {
         '-px': '-1px',
         '-2px': '-2px',
+        '5.5': '1.375rem',
       },
       padding: {
         '-px': '-1px',
-        '-2px': '-2px'
+        '-2px': '-2px',
       },
       backgroundImage: {
-        'spinning-stars': "url('../images/spinning-stars.png')",
-        'object-over-earth': "url('../images/pexels-spacex-23789.png')",
+        'spinning-stars': "linear-gradient(rgba(0,0,0,0.63),rgba(0,0,0,0.63)),url('../images/spinning-stars-orange.png')",
+        'object-over-earth': "linear-gradient(rgba(0,0,0,0.63),rgba(0,0,0,0.63)),url('../images/pexels-spacex-23789.png')",
       }
     },
     screens: {
